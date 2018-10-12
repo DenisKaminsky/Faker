e5715d88fca8f50eab968b127220567c9f1d5e50
DenisKaminsky <deniskaminsky123@mail.ru>
2018-10-12 11:09:57 +0300

    Faker UnitTest add

---
+++ FakerUnitTest/FakerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using FackerProgram;
+using Faker;
+
+namespace FakerUnitTest
+{
+    [TestClass]
+    public class FakerTest
+    {
+        private FackerProgram.Faker _faker;
+        private MyTestClass test;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _faker = new FackerProgram.Faker();
+            _faker.DTOAdd(typeof(Foo));
+            _faker.DTOAdd(typeof(Bar));
+            test = _faker.Create<MyTestClass>();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
